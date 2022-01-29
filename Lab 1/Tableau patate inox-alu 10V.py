5755be6f6fb0731c09bf950bf82504341296bafd
Xavier Mathieu <XAMAT2@ulaval.ca>
2022-01-28 19:20:44 -0500

    Essai format graphique

--- Lab 1/Tableau patate inox-alu 10V.py
+++ Lab 1/Tableau patate inox-alu 10V.py
@@ -3,7 +3,9 @@ import numpy as np
 import lvm_read as l
 
 a = l.read(rf'C:/DATA/Université/Électronique et mesures/Lab 1/Données brutes Lab1/Labo1_Alu-inox_10V_28_01_2022.lvm')
-Tension = a[0]["data"]
-print(a)
-print(Tension)
+Tension = a[0]["data"]*1000
+n_bins = 20
+dist1 = Tension
 
+plt.hist(dist1, bins=n_bins)
+plt.show
