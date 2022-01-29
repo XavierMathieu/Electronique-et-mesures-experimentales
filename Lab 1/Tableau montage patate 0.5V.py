6ef74459da94cae5148034d6e5f4974aa71f0136
Xavier Mathieu <XAMAT2@ulaval.ca>
2022-01-28 20:29:54 -0500

    Création tableau montage patate

--- Lab 1/Tableau montage patate 0.5V.py
+++ Lab 1/Tableau montage patate 0.5V.py
@@ -4,6 +4,20 @@ import numpy as np
 import lvm_read as l
 
 a = l.read(rf'C:/DATA/Université/Électronique et mesures/Lab 1/Données brutes Lab1/Labo1_Montage_0.5V_28_01_2022.lvm')
-Tension = a[0]["data"]
-print(a)
-print(Tension)
+Tensions = a[0]["data"]*1000
+patate = Tensions[:, 0]
+courant = Tensions[:, 1]/12
+n_bins = 20
+
+plt.hist(patate, bins=n_bins, color="k")
+plt.minorticks_on()
+plt.xlabel("Tension mesurée (mV)")
+plt.ylabel("Nombre de données")
+plt.show
+# %%
+plt.hist(courant, bins=n_bins, color="k")
+plt.minorticks_on()
+plt.xlabel("Courant mesuré (mA)")
+plt.ylabel("Nombre de données")
+plt.show
+# %%
