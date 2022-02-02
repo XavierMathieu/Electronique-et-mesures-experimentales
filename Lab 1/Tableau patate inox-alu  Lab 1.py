7028a7a291ab70ed2c2ab85c2988d8e7be1cdf3c
Xavier Mathieu <XAMAT2@ulaval.ca>
2022-02-02 17:29:51 -0500

    Enregistrement et fermer fonction sauvegarde

--- Lab 1/Tableau patate inox-alu  Lab 1.py
+++
@@ -1,8 +0,0 @@
-import matplotlib.pyplot as plt
-import lvm_read as l
-
-a = l.read(rf'C:/DATA/Université/Électronique et mesures/Lab 1/Données brutes Lab1/Labo1_Alu-inox_10V_28_01_2022.lvm')
-Tension = a[0]["data"]
-print(a)
-print(Tension)
-
