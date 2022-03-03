770d1247e6d7c65c1c1fe50bd7ef8bef97382e1d
Xavier Mathieu <XAMAT2@ulaval.ca>
2022-03-02 19:44:49 -0500

    Ajout bases graphiques

--- Lab 5/Graphique tension résistance condensateurs.py
+++ Lab 5/Graphique tension résistance condensateurs.py
@@ -16,13 +16,34 @@ for i in range(15):
     
     V = np.mean(data[:, 1]*1000)
     I_V = np.std(data[:, 1]*1000)/math.sqrt(30)
-
-    print(f"{R} ± {I_R}, {V} ± {I_V}")
+    
     Données += [[R, I_R, V, I_V]]
 
 Données = np.array(Données)
 
+Res = np.sort(Données[:, 0])
+I_Res = np.sort(Données[:, 1])
+Ten = np.sort(Données[:, 2])
+I_Ten = np.sort(Données[:, 3])
 
+#%%
+plt.style.use('https://raw.githubusercontent.com/dccote/Enseignement/master/SRC/dccote-errorbars.mplstyle')
+fig=plt.figure(figsize=(6.4, 4.8*1.2))
+fig.subplots_adjust(bottom=0.3)
+
+
+plt.errorbar(Res, Ten, xerr = I_Res, yerr = I_Ten, mfc = "black", ms = 4, ls = "-")
+plt.xlabel("Résistance (Ohm)")
+plt.ylabel("Tension efficace (mV)")
+
+caption = """
+Figure 2:  
+"""
+
+plt.text(0.07, 0.2, caption, fontsize='x-large', verticalalignment='top', transform=plt.gcf().transFigure)
+
+plt.show
+#plt.savefig(rf"C:/DATA/Université/Électronique et mesures/Lab 3/Graphique I-V bobine.pdf")
 
 
 
