fd5e7af0cb8e15d4a14f6ddc8487048c58ebac6c
Xavier Mathieu <XAMAT2@ulaval.ca>
2022-03-18 12:23:52 -0400

    Tri automatique des valeurs

--- Lab 5/Graphique tension résistance condensateurs.py
+++ Lab 5/Graphique tension résistance condensateurs.py
@@ -14,30 +14,43 @@ for i in range(15):
     R = np.mean(data[:, 0])
     I_R = np.std(data[:, 0])/math.sqrt(30)
     
-    V = np.mean(data[:, 1]*1000)
-    I_V = np.std(data[:, 1]*1000)/math.sqrt(30)
+    V = np.mean(data[:, 1])
+    I_V = np.std(data[:, 1])/math.sqrt(30)
 
-    Données += [[R, I_R, V, I_V]]
+    P = (V**2)/R
+    I_P = math.sqrt(2*(I_V/V)**2 + (I_R/R)**2)*P
+
+    mesure = [R, I_R, P, I_P]
+
+    Ajout = True
+
+    for j in Données:
+        if j[0] > R:
+            Données.insert(Données.index(j), mesure)
+            Ajout = False
+            break
+    if Ajout:
+        Données += [mesure]
 
 Données = np.array(Données)
 
-Res = np.sort(Données[:, 0])
-I_Res = np.sort(Données[:, 1])
-Ten = np.sort(Données[:, 2])
-I_Ten = np.sort(Données[:, 3])
+Res = (Données[:, 0])
+I_Res = (Données[:, 1])
+Ten = (Données[:, 2])
+I_Ten = (Données[:, 3])
 
 #%%
 plt.style.use('https://raw.githubusercontent.com/dccote/Enseignement/master/SRC/dccote-errorbars.mplstyle')
 fig=plt.figure(figsize=(6.4, 4.8*1.2))
 fig.subplots_adjust(bottom=0.3)
 
-
+plt.xscale("log")
 plt.errorbar(Res, Ten, xerr = I_Res, yerr = I_Ten, mfc = "black", ms = 4, ls = "-")
 plt.xlabel("Résistance (Ohm)")
 plt.ylabel("Tension efficace (mV)")
 
 caption = """
-Figure 2: Tension efficace du circuit (mV) en
+Figure 2: Puissance en
 fonction de la résistance totale du circuit (Ohm)
 dans la circuit sans condensateurs en parallèle
 """
--- Lab 5/Graphique tension résistance.py
+++ Lab 5/Graphique tension résistance.py
@@ -6,6 +6,7 @@ import lvm_read as l
 import math
 
 Données = []
+Résistances = []
 
 for i in range(15):
     a = l.read(rf'C:/DATA/Université/Électronique et mesures/Lab 5/Labo 5 mesures/Labo 5 Partie 1 resistance seule {i+1}.lvm')
@@ -17,21 +18,34 @@ for i in range(15):
     V = np.mean(data[:, 1]*1000)
     I_V = np.std(data[:, 1]*1000)/math.sqrt(30)
 
-    Données += [[R, I_R, V, I_V]]
+    P = (V**2)/R
+    I_P = math.sqrt(2*(I_V/V)**2 + (I_R/R)**2)*P
+
+    mesure = [R, I_R, P, I_P]
+
+    Ajout = True
+
+    for j in Données:
+        if j[0] > R:
+            Données.insert(Données.index(j), mesure)
+            Ajout = False
+            break
+    if Ajout:
+        Données += [mesure]
 
 Données = np.array(Données)
 
-Res = np.sort(Données[:, 0])
-I_Res = np.sort(Données[:, 1])
-Ten = np.sort(Données[:, 2])
-I_Ten = np.sort(Données[:, 3])
+Res = (Données[:, 0])
+I_Res = (Données[:, 1])
+Ten = (Données[:, 2])
+I_Ten = (Données[:, 3])
 
 #%%
 plt.style.use('https://raw.githubusercontent.com/dccote/Enseignement/master/SRC/dccote-errorbars.mplstyle')
 fig=plt.figure(figsize=(6.4, 4.8*1.2))
 fig.subplots_adjust(bottom=0.3)
 
-
+plt.xscale("log")
 plt.errorbar(Res, Ten, xerr = I_Res, yerr = I_Ten, mfc = "black", ms = 4, ls = "-")
 plt.xlabel("Résistance (Ohm)")
 plt.ylabel("Tension efficace (mV)")
