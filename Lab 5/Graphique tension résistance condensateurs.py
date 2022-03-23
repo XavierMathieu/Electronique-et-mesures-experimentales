c796ca0370c25bde605f562331bab75a0ca65dbc
Xavier Mathieu <XAMAT2@ulaval.ca>
2022-03-23 18:28:47 -0400

    Ajout courbes de tendance

--- Lab 5/Graphique tension résistance condensateurs.py
+++ Lab 5/Graphique tension résistance condensateurs.py
@@ -1,5 +1,6 @@
 #%%
-from turtle import fillcolor
+from operator import index
+from turtle import color, fillcolor
 import matplotlib.pyplot as plt
 import numpy as np
 import lvm_read as l
@@ -20,7 +21,7 @@ for i in range(20):
     P = (V**2)/R
     I_P = math.sqrt(2*(I_V/V)**2 + (I_R/R)**2)*P
 
-    mesure = [R, I_R, (P*1000), (I_P*1000)]
+    mesure = [R, I_R, (P), (I_P)]
 
     Ajout = True
 
@@ -31,9 +32,10 @@ for i in range(20):
             break
     if Ajout:
         Données += [mesure]
-    print(f"{i+1} & {R} ± {I_R} & {V} ± {I_V} & {P} ± {I_P}" + "\\" + "\\")
+    #print(f"{i+1} & {R} ± {I_R} & {V} ± {I_V} & {P} ± {I_P}" + "\\" + "\\")
 
 Données = np.array(Données)
+print(Données)
 
 Res = (Données[:, 0])
 I_Res = (Données[:, 1])
@@ -41,15 +43,37 @@ Ten = (Données[:, 2])
 I_Ten = (Données[:, 3])
 
 #%%
+from scipy import optimize
+
+def Puissance(R_ch, R_s, V_s):
+    return (R_ch*(V_s)**2)/((R_ch+R_s)**2)
+
+#%%
 plt.style.use('https://raw.githubusercontent.com/dccote/Enseignement/master/SRC/dccote-errorbars.mplstyle')
 fig=plt.figure(figsize=(6.4, 4.8*1.2))
 fig.subplots_adjust(bottom=0.3)
 
+Res_ref = np.linspace(10, 220 , 2200)
+Pui_ref = np.linspace(0, 2.2, 2200)
+
 plt.xscale("log")
-plt.errorbar(Res, Ten, xerr = I_Res, yerr = I_Ten, mfc = "black", ms = 4, ls = "-")
+plt.errorbar(Res, Ten*1000, xerr = I_Res, yerr = I_Ten*1000, mfc = "black", ms = 4, ls = "-", label="Puissance réele")
+
+opt = optimize.curve_fit(Puissance, Res[4:], Ten[4:])
+R_s = opt[0][0]
+V_s = opt[0][1]
+err = np.sqrt(np.diag(opt[1]))
+print(f"Résistance de la source: {R_s} ± {err[0]}")
+print(f"Tension de la source: {V_s} ± {err[1]}")
+
+ref = Puissance(Res_ref, R_s, V_s)
+
+plt.plot(Res_ref, Puissance(Res_ref, R_s, V_s)*1000, color = "red", ms = 0, ls = "-", label = "Courbe théorique")
+plt.plot(Res_ref[list(ref).index(max(ref))]*np.ones(2200), Pui_ref, color="blue", ms = 0, ls = "-", label = f"Résistance de la source")
+
 plt.xlabel("Résistance [Ohm]")
 plt.ylabel("Puissance dissipée [mW]")
-
+plt.legend()
 #caption = """Figure 2: Puissance en fonction de la résistance totale du circuit (Ohm) dans la circuit sans condensateurs en parallèle"""
 
 #plt.text(0.07, 0.2, caption, fontsize='x-large', verticalalignment='top', transform=plt.gcf().transFigure)
--- Lab 5/Graphique tension résistance.py
+++ Lab 5/Graphique tension résistance.py
@@ -21,7 +21,7 @@ for i in range(20):
     P = (V**2)/R
     I_P = math.sqrt(2*(I_V/V)**2 + (I_R/R)**2)*P
 
-    mesure = [R, I_R, (P*1000), (I_P*1000)]
+    mesure = [R, I_R, (P), (I_P)]
 
     Ajout = True
 
@@ -32,7 +32,7 @@ for i in range(20):
             break
     if Ajout:
         Données += [mesure]
-    print(f"{R} ± {I_R} & {V} ± {I_V} & {P} ± {I_P}")
+    #print(f"{R} ± {I_R} & {V} ± {I_V} & {P} ± {I_P}")
 
 Données = np.array(Données)
 
@@ -42,15 +42,34 @@ Ten = (Données[:, 2])
 I_Ten = (Données[:, 3])
 
 #%%
+from scipy import optimize
+
+def Puissance(R_ch, R_s):
+    return (R_ch)/(2*(R_ch+R_s)**2)
+
+#%%
 plt.style.use('https://raw.githubusercontent.com/dccote/Enseignement/master/SRC/dccote-errorbars.mplstyle')
 fig=plt.figure(figsize=(6.4, 4.8*1.2))
 fig.subplots_adjust(bottom=0.3)
 
+Res_ref = np.linspace(10, 250 , 2500)
+Pui_ref = np.linspace(0, 2.5, 2500)
+
 plt.xscale("log")
-plt.errorbar(Res, Ten, xerr = I_Res, yerr = I_Ten, mfc = "black", ms = 4, ls = "-")
+plt.errorbar(Res, Ten*1000, xerr = I_Res, yerr = I_Ten*1000, mfc = "black", ms = 4, ls = "-", label="Puissance réele")
+
+opt = optimize.curve_fit(Puissance, Res[7:], Ten[7:])
+R_s = opt[0][0]
+err = np.sqrt(np.diag(opt[1]))
+print(f"Résistance de la source: {R_s} ± {err[0]}")
+
+ref = Puissance(Res_ref, R_s)
+
+plt.plot(Res_ref, Puissance(Res_ref, R_s)*1000, color = "red", ms = 0, ls = "-", label = "Courbe du modèle théorique")
+plt.plot(Res_ref[list(ref).index(max(ref))]*np.ones(2500), Pui_ref, color="blue", ms = 0, ls = "-", label = f"Résistance de la source")
 plt.xlabel("Résistance (Ohm)")
 plt.ylabel("Puissance dissipée (mW)")
-
+plt.legend()
 #caption = """Figure 1: Tension efficace du circuit (mV) en fonction de la résistance totale du circuit (Ohm) dans la circuit sans condensateurs en parallèle"""
 
 #plt.text(0.07, 0.2, caption, fontsize='x-large', verticalalignment='top', transform=plt.gcf().transFigure)
